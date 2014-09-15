e7acf6ee97157a95f021cdd5115bd7dd8cbfe562
John Diegor <john.diegor@gmail.com>
2014-09-15 13:50:20 +0800

    cleanup

--- test/controllers/charges_controller_test.rb
+++ test/controllers/charges_controller_test.rb
@@ -35,46 +35,4 @@ class ChargesControllerTest < ActionController::TestCase
       assert true
     end
   end
-
-  #test "should get index" do
-  #  get :index
-  #  assert_response :success
-  #  assert_not_nil assigns(:charges)
-  #end
-
-  #test "should get new" do
-  #  get :new
-  #  assert_response :success
-  #end
-
-  #test "should create charge" do
-  #  assert_difference('Charge.count') do
-  #    post :create, charge: { amount: @charge.amount, currency: @charge.currency, customer_id: @charge.customer_id, disputed: @charge.disputed, paid: @charge.paid, refunded: @charge.refunded }
-  #  end
-
-  #  assert_redirected_to charge_path(assigns(:charge))
-  #end
-
-  #test "should show charge" do
-  #  get :show, id: @charge
-  #  assert_response :success
-  #end
-
-  #test "should get edit" do
-  #  get :edit, id: @charge
-  #  assert_response :success
-  #end
-
-  #test "should update charge" do
-  #  patch :update, id: @charge, charge: { amount: @charge.amount, currency: @charge.currency, customer_id: @charge.customer_id, disputed: @charge.disputed, paid: @charge.paid, refunded: @charge.refunded }
-  #  assert_redirected_to charge_path(assigns(:charge))
-  #end
-
-  #test "should destroy charge" do
-  #  assert_difference('Charge.count', -1) do
-  #    delete :destroy, id: @charge
-  #  end
-
-  #  assert_redirected_to charges_path
-  #end
 end
