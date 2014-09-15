ce8c45edd16ba2c2ef7e45bfff95eded5843499f
John Diegor <john.diegor@gmail.com>
2014-09-15 13:10:21 +0800

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :charges
 
   resources :customers
+  root to:  'charges#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
